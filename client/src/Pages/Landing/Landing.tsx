6e71c0a245750fd0f55848784503a2608ee6e5f3
Fallenefc <arylmoraesn@hotmail.com>
2020-11-14 08:49:33 -0600

    style: finished landing page

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -21,7 +21,7 @@ export default function Landing(): ReactElement {
             <span>Contact</span>
           </div>
           <div className='middle-content'>
-            <img src={logo} height='50px'></img>
+            <img src={logo} height='70px'></img>
           </div>
           <div className='right-content'>
             <button className='login-btn'>Log In</button>
@@ -32,15 +32,70 @@ export default function Landing(): ReactElement {
           <div className='landing-text'>
             <h2>Short title description goes brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores cumque minus amet cupiditate molestias, ut quo officia dolor, veniam sed vel doloremque corporis nihil et itaque fuga numquam expedita quis.</p>
-            <button className='login-btn'>Log In</button>
+            <button className='login-btn' id="bottom-login">Log In</button>
             <button className='signup-btn'>Sign Up</button>
           </div>
           <div className='landing-image'>
             <img src={landingHero} height='500px'></img>
           </div>
         </div>
-        <footer>
-          hey
+        <footer className='footer-distributed'>
+        <div className="footer-left">
+        {/* <img src="img/logo.png"></img> */}
+        <h3>
+          About<span> Me</span>
+        </h3>
+
+        <p className="footer-links">
+          <a href="#">Home</a>|<a href="#">About</a>|<a href="#">Contact</a>
+        </p>
+
+        <p className="footer-company-name">
+          © 2020 FallenEFC
+        </p>
+      </div>
+      <div className="footer-center">
+        <div>
+          <i className="fa fa-map-marker"></i>
+          <p>
+            <span>blablabla</span>
+          </p>
+        </div>
+
+        <div>
+          <i className="fa fa-phone"></i>
+          <p>Phone?</p>
+        </div>
+        <div>
+          <i className="fa fa-envelope"></i>
+          <p>
+            <a href="mailto:support@eduonix.com">arylmoraesn@gmail.com</a>
+          </p>
+        </div>
+      </div>
+      <div className="footer-right">
+        <p className="footer-company-about">
+          <span>About Me</span>
+          Web developer
+        </p>
+        <div className="footer-icons">
+          <a href="#">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="#">
+          <i className="fa fa-github"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-youtube"></i>
+          </a>
+        </div>
+      </div>
         </footer>
       </div>
     </div>
