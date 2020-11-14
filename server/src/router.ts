db18f2f60e00d54e1c82a8e48da6e86c74f1420b
Fallenefc <arylmoraesn@hotmail.com>
2020-11-13 18:10:43 -0600

    commented and organised routes.ts

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,12 @@
 import express from "express";
-import { addQuestionToExam, deleteAnExam, deleteQuestionFromExam, generateExam, getExams, getFullExam } from "./controllers/exams";
+import {
+  addQuestionToExam,
+  deleteAnExam,
+  deleteQuestionFromExam,
+  generateExam,
+  getExams,
+  getFullExam,
+} from "./controllers/exams";
 import {
   deleteQuestion,
   getQuestions,
@@ -18,35 +25,37 @@ import authMiddleware from "./middleware/auth";
 const router = express.Router();
 
 // USER Auth Routes
-router.post("/signup", registerUser); // ok!
-router.post("/login", userLogIn); // ok!
-router.post("/forgotPassword", forgotPassword); // ok!
-router.post("/resetPassword", resetPassword); // ok!
-router.post("/logout", () => console.log("I am placeholder")); // this wont do anything for now, but I will destroy the token on the client side
+// Login and Signup
+router.post("/signup", registerUser);
+router.post("/login", userLogIn);
+
+// Forgot password, Reset Password, Logout
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword", resetPassword);
+// router.post("/logout", () => console.log("I am placeholder")); // this wont do anything for now, but I will destroy the token on the client side
 router.get("/me", authMiddleware, getUserInfo);
 
 // QUESTION BANK ROUTES
-router.get("/questions", authMiddleware, getQuestions); // ok!
-router.post("/questions", authMiddleware, postQuestion); // ok!
-router.put("/questions/:id", authMiddleware, updateQuestion); // ok!
-router.delete("/questions/:id", authMiddleware, deleteQuestion); // ok!
+// Get, Post, Update and Delete questions. Pretty straight forward
+router.get("/questions", authMiddleware, getQuestions);
+router.post("/questions", authMiddleware, postQuestion);
+router.put("/questions/:id", authMiddleware, updateQuestion);
+router.delete("/questions/:id", authMiddleware, deleteQuestion);
 
 // EXAM ROUTES
-// create a verify route
-router.post("/exams", authMiddleware, generateExam); // this will create an exam (empty) - ok!
-router.post("/addQuestion", authMiddleware, addQuestionToExam); // this will add a question to the exam - ok!
-router.post("/deleteQuestion", authMiddleware, deleteQuestionFromExam); // this will delete a question from the exam
+// Exam itself routes: Get (all), Get(one), Create, Delete and Generate an exam(after it is done)
+router.get("/exams", authMiddleware, getExams);
+router.get("/singleExam/:examId", authMiddleware, getFullExam);
+router.post("/exams", authMiddleware, generateExam);
+router.delete("/exams/:examId", authMiddleware, deleteAnExam);
 router.post("/generateExam", authMiddleware, () =>
   console.log("I am placeholder")
-); // this will generate an exam after all the questions are added
+);
 
-// Route that gets the exam and all the full questions inside
-router.get('/singleExam/:examId', authMiddleware, getFullExam); // working
+// Modifying/Adding questions in an exam Routes
+router.post("/addQuestion", authMiddleware, addQuestionToExam);
+router.post("/deleteQuestion", authMiddleware, deleteQuestionFromExam);
 
-router.get("/exams", authMiddleware, getExams); // ok
-router.delete("/exams/:examId", authMiddleware, deleteAnExam); // this will delete an exam - apparently its ok
-// Exam results
-// must create a POST route that sends the completed exam?
 router.get("/results", () => console.log("I am placeholder"));
 
 export default router;
